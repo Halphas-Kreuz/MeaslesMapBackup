d52fa18868bf89a78c25abd009dac43a75586865
Halphas <halphas.kreuz@gmail.com>
2023-01-06 20:51:39 +0100

    update for testdata

---
+++ run_split.py
@@ -0,0 +1,9 @@
+f = open("Deutschland n=200-Tabelle 1.csv","r")
+L = f.readlines()
+L = L[2:]
+for s in L:
+    l = s.split(";")
+    x = l[2]
+    code = x.split()[0]
+    pos = " ".join(x.split()[1:])
+    print(f"{l[0]};{l[1]};{code};{code[0]}{code[1]};{pos.split(',')[0]};{pos.split(',')[1][1:]};{l[3]};{l[4]}", end="")
