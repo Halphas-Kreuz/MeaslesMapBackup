951aa8cf80f64123c00ce60d4b422142e8c4bc75
Halphas <halphas.kreuz@gmail.com>
2023-01-10 16:18:48 +0100

    B200 data update

--- run_split.py
+++ run_split.py
@@ -1,4 +1,4 @@
-f = open("Deutschland n=200-Tabelle 1.csv","r")
+f = open("Berlin n=200-Tabelle 1.csv","r")
 L = f.readlines()
 L = L[2:]
 for s in L:
@@ -6,4 +6,5 @@ for s in L:
     x = l[2]
     code = x.split()[0]
     pos = " ".join(x.split()[1:])
+    l[-1] = l[-1].replace("ErkrBeginn",0)
     print(f"{l[0]};{l[1]};{code};{code[0]}{code[1]};{pos.split(',')[0]};{pos.split(',')[1][1:]};{l[3]};{l[4]}", end="")
